5b97d64a8e30687b237d130fa9e67a8fbabee7c6
Josh Malters <frostymalts@gmail.com>
2014-09-05 20:25:05 -0600

    Crazy!
    So many things done!!!

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,9 +1,19 @@
 
 var main = function() {
-    $("#target").keypress(function(event){
-        var current = $("")
+    $(".body").keypress(function(event){
+        var current = $(".active");
+        var next = current.next();
+        var prev = current.prev();
+
+        if(next.length() === 0) {
+            next = $(".li").first();
+        }
+        if (prevSlide.length === 0) {
+            prevSlide = $('.li').last();
+        }
         if(event.which === 39) {
-            $('.btn').addClass("disabled")
+            current.fadeOut(600).removeClass("active");
+            next.fadeIn(600).addClass("active");
         } else if (event.which === 37) {
 
         }
